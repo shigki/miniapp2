85a78e0c7b84cba27f352ee9fd469d738e126ae9
内田茂樹 <shigeki.uchida@gmail.com>
2018-08-01 12:15:53 +0900

    途中

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,18 @@
 class Blog < ActiveRecord::Base
 	belongs_to :user
 	validates :title, :text, :image, presence: true
+	validate :add_error_sample
+
+  def add_error_sample
+    # nameが空のときにエラーメッセージを追加する
+    if title.empty?
+      errors.add(:title, "を入力してください")
+    end
+    if image.empty?
+      errors.add(:image, "を入力してください")
+    end
+    if text.empty?
+      errors.add(:text, "を入力してください")
+    end
+  end
 end
